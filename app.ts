b75fc00b2f7dacf1e17a72f76f1f9e8c5828fe22
RilusMahmud <rilusmahmud@live.com>
2024-05-12 01:11:54 +0600

    feat: added kinde auth WIP

--- app.ts
+++ app.ts
@@ -2,6 +2,7 @@ import { Hono } from "hono";
 import { logger } from "hono/logger";
 
 import { expensesRoute } from "./routes/expenses";
+import { authRoute } from "./routes/auth";
 
 const app = new Hono();
 
@@ -11,6 +12,12 @@ app.get("/", (c) => {
   return c.json({ message: "Hello, World!" });
 });
 
-app.route("/api/v1/expenses", expensesRoute);
+// app.route("/api/v1/expenses", expensesRoute).route("/", authRoute);
+
+const apiRoutes = app
+  .route("/api/v1")
+  .route("/expenses", expensesRoute)
+  .route("/", authRoute);
 
 export default app;
+export type ApiRoutes = typeof apiRoutes;
--- index.ts
+++ index.ts
@@ -1,6 +1,7 @@
 import app from "./app";
 
 Bun.serve({
+  hostname: "0.0.0.0",
   fetch: app.fetch,
 });
 
