7aac59e952be4737f3a7ecb70d2c819f93b8774c
RilusMahmud <rilusmahmud@live.com>
2024-05-18 00:08:58 +0600

    feat: added build and serve config from hono backend

--- server/app.ts
+++ server/app.ts
@@ -1,4 +1,5 @@
 import { Hono } from "hono";
+import { serveStatic } from "hono/bun";
 import { logger } from "hono/logger";
 
 import { expensesRoute } from "./routes/expenses";
@@ -8,16 +9,13 @@ const app = new Hono();
 
 app.use("*", logger());
 
-app.get("/", (c) => {
-  return c.json({ message: "Hello, World!" });
-});
-
-// app.route("/api/v1/expenses", expensesRoute).route("/", authRoute);
-
 const apiRoutes = app
   .route("/api/v1")
   .route("/expenses", expensesRoute)
   .route("/", authRoute);
+
+app.get("*", serveStatic({ root: "./frontend/dist" }));
+app.get("*", serveStatic({ path: "./frontend/dist/index.html" }));
 
 export default app;
 export type ApiRoutes = typeof apiRoutes;
