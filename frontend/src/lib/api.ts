4ca7e3c84f47d416b58a9f74b593ae33027553b8
RilusMahmud <rilusmahmud@live.com>
2024-05-22 12:50:18 +0600

    feat: added user query option with staletime infinity

--- frontend/src/lib/api.ts
+++ frontend/src/lib/api.ts
@@ -1,6 +1,22 @@
 import { hc } from "hono/client";
 import { type ApiRoutes } from "@server/app";
+import { queryOptions } from "@tanstack/react-query";
 
 const client = hc<ApiRoutes>("/");
 
 export const api = client.api;
+
+async function getCurrentUser() {
+  const res = await api.v1.me.$get();
+  if (!res.ok) {
+    throw new Error("Failed to fetch total spent");
+  }
+  const data = await res.json();
+  return data;
+}
+
+export const userQueryOptions = queryOptions({
+  queryKey: ["get-current-user"],
+  queryFn: getCurrentUser,
+  staleTime: Infinity,
+});
