9293f5d4d6e6e34e211d24fd143083eb693da792
RilusMahmud <rilusmahmud@live.com>
2024-05-24 00:51:33 +0600

    feat: added date props for the create and get expenses

--- frontend/src/routes/_authenticated/create-expenses.tsx
+++ frontend/src/routes/_authenticated/create-expenses.tsx
@@ -2,6 +2,7 @@ import { createFileRoute, useNavigate } from "@tanstack/react-router";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { Button } from "@/components/ui/button";
+import { Calendar } from "@/components/ui/calendar";
 
 import { useForm } from "@tanstack/react-form";
 
@@ -22,6 +23,7 @@ function CreateExpenses() {
     defaultValues: {
       title: "",
       amount: "0",
+      date: new Date().toISOString(),
     },
     onSubmit: async ({ value }) => {
       // Do something with form data
@@ -79,6 +81,27 @@ function CreateExpenses() {
                 onBlur={field.handleBlur}
                 type="number"
                 onChange={(e) => field.handleChange(e.target.value)}
+              />
+              {field.state.meta.touchedErrors ? (
+                <em>{field.state.meta.touchedErrors}</em>
+              ) : null}
+            </div>
+          )}
+        />
+        <form.Field
+          name="date"
+          validators={{
+            onChange: createExpenseSchema.shape.date,
+          }}
+          children={(field) => (
+            <div className="self-center">
+              <Calendar
+                mode="single"
+                selected={new Date(field.state.value)}
+                onSelect={(date) =>
+                  field.handleChange((date ?? new Date()).toISOString())
+                }
+                className="rounded-md border"
               />
               {field.state.meta.touchedErrors ? (
                 <em>{field.state.meta.touchedErrors}</em>
